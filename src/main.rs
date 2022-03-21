d8f789d81f6b1c3bfa1728203ddae4788ca4c0c1
gicrisf <giovanni.crisalfi@protonmail.com>
2022-03-21 03:47:26 +0100

    Chart component

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,115 @@ use gtk::{
     },
     Orientation,
 };
-use relm4::{adw, gtk, send, AppUpdate, RelmComponent, Model, RelmApp, Sender, Widgets};
+// use relm4::{adw, gtk, send, AppUpdate, RelmComponent, ComponentUpdate, Model, RelmApp, Sender, Widgets};
+use relm4::*;
+// use relm4::factory::FactoryVecDeque;
+
+// -- Entities
+
+pub struct Param {
+    pub val: f64,  // Value; starts with 0.0
+    pub var: f64,  // Variation; starts with: 0.0
+}
+
+pub struct Radical {
+    pub lwa: Param,  // Line width A
+    // pub lwb: Param,
+    // pub lwc: Param,
+    pub lrtz: Param,  // Lorentzian linewidth parameter (%)
+    pub amount: Param,  // Relative amount
+    pub dh1: Param,
+    // pub nucs: Vec<Nucleus>,
+}
 
 #[derive(Default)]
 struct AppModel {
-    counter: u8,
+    empirical: Option<Vec<f64>>,
+    theoretical: Option<Vec<f64>>,
+    points: f64,
+    sweep: f64,
+    // params: FactoryVecDeque<Radical>,  // TODO implement Radical
+    sigma: f64,
+    iters: usize,
     montecarlo: bool,
 }
 
 enum AppMsg {
-    Increment,
-    Decrement,
     Montecarlo(bool),
 }
 
-// #[derive(relm4::Components)]
-// struct AppComponents {}
+// -- Chart model
+
+#[derive(Default)]
+struct ChartModel {}
+
+enum ChartMsg {
+    Draw,
+}
+
+impl Model for ChartModel {
+    type Msg = ChartMsg;
+    type Widgets = ChartWidgets;
+    type Components = ();
+}
+
+impl ComponentUpdate<AppModel> for ChartModel {
+    fn init_model(_parent_model: &AppModel) -> Self {
+        ChartModel {}
+    }
+
+    fn update(
+        &mut self,
+        msg: ChartMsg,
+        _components: &(),
+        _sender: Sender<ChartMsg>,
+        parent_sender: Sender<AppMsg>,
+    ) {
+        match msg {
+            ChartMsg::Draw => {
+                // Draw
+            }
+        }
+    }
+}
+
+// -- Chart Widgets
+
+#[relm4::widget]
+impl Widgets<ChartModel, AppModel> for ChartWidgets {
+    view! {
+        gtk::Box {
+            set_orientation: gtk::Orientation::Vertical,
+            set_spacing: 10,
+            set_hexpand: true,
+            append = &gtk::Label {
+                set_label: "Left-click to add circles, resize or right-click to reset!",
+            },
+            append: area = &gtk::DrawingArea {
+                set_vexpand: true,
+                set_hexpand: true,
+            }  // ./DrawingArea
+        }
+    }
+}
+
+
+// ... Back to AppModel
+
+#[derive(relm4::Components)]
+struct AppComponents {
+    chart: RelmComponent<ChartModel, AppModel>
+}
 
 impl Model for AppModel {
     type Msg = AppMsg;
     type Widgets = AppWidgets;
-    // type Components = AppComponents;
-    type Components = ();
+    type Components = AppComponents;
 }
 
 impl AppUpdate for AppModel {
-    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
+    fn update(&mut self, msg: AppMsg, _components: &AppComponents, _sender: Sender<AppMsg>) -> bool {
         match msg {
-            AppMsg::Increment => {
-                self.counter = self.counter.wrapping_add(1);
-            }
-            AppMsg::Decrement => {
-                self.counter = self.counter.wrapping_sub(1);
-            }
             AppMsg::Montecarlo(v) => {
                 self.montecarlo = v;
             }
@@ -72,18 +148,18 @@ impl Widgets<AppModel, ()> for AppWidgets {
                         append = &gtk::Button {
                             set_label: "Increase",
                             connect_clicked(sender) => move |_| {
-                                send!(sender, AppMsg::Increment)
+                                // send!(sender, AppMsg::Increment)
                             }
                         },
                         append = &gtk::Button {
                             set_label: "Decrease",
                             connect_clicked(sender) => move |_| {
-                                send!(sender, AppMsg::Decrement)
+                                // send!(sender, AppMsg::Decrement)
                             }
                         },
                     } -> params_page: ViewStackPage {
                         set_icon_name: Some("document-print-symbolic"),
-                        set_badge_number: watch!(model.counter as u32),
+                        // set_badge_number: watch!(model.counter as u32),
                     },
                     add_titled(Some("Plot"), "Plot") = &gtk::Box {
                         set_orientation: Orientation::Vertical,
@@ -91,6 +167,8 @@ impl Widgets<AppModel, ()> for AppWidgets {
                         append = &gtk::Label {
                             set_label: "This is the plotting page"
                         },
+                        // append: component!(Some(chart)),  // Still not supported macro?
+                        append: components.chart.root_widget(),
                         append = &gtk::ToggleButton {
                             set_label: "Run MonteCarlo",
                             set_active: model.montecarlo,
@@ -120,8 +198,14 @@ impl Widgets<AppModel, ()> for AppWidgets {
 
 fn main() {
     let model = AppModel {
-        counter: 5,
-        montecarlo: true,
+        empirical: None,
+        theoretical: None,
+        points: 1024.0,
+        sweep: 100.0,
+        // params: FactoryVecDeque::new(),
+        sigma: 1e+20,
+        iters: 0,
+        montecarlo: false,
     };
     let app = RelmApp::new(model);
     app.run();
