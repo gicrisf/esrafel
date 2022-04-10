dfa64e326d85436018dd74561ce8c251f7068a17
gicrisf <giovanni.crisalfi@protonmail.com>
2022-04-10 20:32:18 +0200

    Improve, simplify About component and fill fields

--- src/about.rs
+++ src/about.rs
@@ -4,22 +4,6 @@ use gtk::prelude::{WidgetExt, GtkWindowExt};
 
 pub struct AboutModel {
     is_active: bool,
-    // authors: Vec<String>,
-    // artists: Vec<String>,
-    comments: String,  // TODO static &str
-    // copyright: String,
-    // documenters: Vec<String>,
-    // license: String,
-    // license_type: gtk::License,
-    // logo: gtk::Paintable,
-    // logo_icon_name: String,
-    program_name: String,
-    // system_information: String,
-    // translator_credits: String,
-    version: String,
-    // website: String,
-    // website_label: String,
-    // wrap_license: book,
 }
 
 pub enum AboutMsg {
@@ -37,10 +21,6 @@ impl ComponentUpdate<AppModel> for AboutModel {
     fn init_model(_parent_model: &AppModel) -> Self {
         AboutModel {
             is_active: false,
-            // authors: vec!["Giovanni Crisalfi".into()],
-            comments: "Yeah".into(),
-            program_name: "ESRafel".into(),
-            version: "0.1.0".into(),
         }
     }
 
@@ -64,19 +44,27 @@ impl Widgets<AboutModel, AppModel> for AboutWidgets {
         win = gtk::AboutDialog {
             set_visible: watch!(model.is_active),
             set_modal: true,
-            // set_authors: &model.authors,
-            set_comments: Some(&model.comments),
-            // set_copyright: Some(&model.copyright),
-            set_program_name: Some(&model.program_name),
-            // set_license: Some(&model.license),
-            set_version: Some(&model.version),
+            // set_artists: &["Giovanni Crisalfi"],
+            set_authors: &["Giovanni Crisalfi"],
+            set_comments: Some("Software for least-squares fitting of ESR/EPR spectra with Monte Carlo methods."),
+            set_copyright: Some("© 2022 Giovanni Crisalfi"),
+            // set_documenters: &["Giovanni Crisalfi"],
+            set_license: Some("GPL3"),
+            set_license_type: gtk::License::Gpl30,
+            // set_logo: ,
+            // set_logo_icon_name: ,
+            set_program_name: Some("ESRafel"),
+            // set_system_information: ?,
+            // set_translator_credits: Some("Giovanni Crisalfi"),
+            set_version: Some("0.1.0"),
+            set_website: Some("https://www.github.com/gicrisf/esrafel/"),
+            set_website_label: "github.com/gicrisf/esrafel",
         }
     }  // view macro
 
     fn pre_view() {
         let sender1 = sender.clone();
         self.root_widget().connect_close_request(move |_| {
-            // &model.is_active = false;
             send!(sender1, AboutMsg::Hide);
             gtk::Inhibit(true)
         });
